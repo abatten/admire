bb255d27a4c63ca389c6908d2f92bfeb4b35b23a
abatten <adamjbatten@gmail.com>
2019-01-14 18:20:00 +1100

    Fixed bug in redshift converstion if z or mpc is 0

--- admire/utils.py
+++ admire/utils.py
@@ -3,7 +3,15 @@ import astropy.units as u
 
 
 def z_to_mpc(z):
-    return cosmo.comoving_distance(z)
+
+    if z == 0:
+        return 0 * u.Mpc
+    else:
+        return cosmo.comoving_distance(z)
 
 def mpc_to_z(mpc):
-    return z_at_value(cosmo.comoving_distance, mpc)
+
+    if mpc == 0:
+        return 0
+    else:
+        return z_at_value(cosmo.comoving_distance, mpc)
