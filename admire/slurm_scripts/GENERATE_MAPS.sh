81b7f21638013f446ccae36eb7039479e66910b9
abatten <adamjbatten@gmail.com>
2019-06-25 15:25:05 +1000

    Renamed a bunch of files and moved them around

---
+++ admire/slurm_scripts/GENERATE_MAPS.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=60G
+#SBATCH --time=2:00:0
+
+module load anaconda3/5.0.1
+
+source activate py3
+
+module load gcc/7.3.0
+module load git/2.16.0
+module load openmpi/3.0.0
+module load hdf5/1.10.1
+
+python ../step2_generate_interpolated_maps.py ../param_files/pipeline.params
